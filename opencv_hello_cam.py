fc83dc08717a204f447a0d705d50234a8b64f248
Jack Cho <jack06215@gmail.com>
2021-05-09 10:11:34 +0800

    initial setup

---
+++ opencv_hello_cam.py
@@ -0,0 +1,25 @@
+import sys
+import cv2 as cv
+
+cap = cv.VideoCapture(1)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit(1)
+
+while True:
+    # Capture frame-by-frameq
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    cv.imshow('frame', gray)
+    if cv.waitKey(1) == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
